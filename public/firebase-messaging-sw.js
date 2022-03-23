73eee90bfc7dde1d37119d602f4712d93d42c66e
Shikha Pathak <shikha.pathak6@gmail.com>
2022-03-23 12:33:40 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCgkRfVkuWHOL03eQK4PGxostzq75oVahc",
-			authDomain: "babaweb01239.firebaseapp.com",
-			projectId: "babaweb01239",
-			storageBucket: "babaweb01239.appspot.com",
-			messagingSenderId: "805387171653",
-			appId: "1:805387171653:web:211db03f58a83b7c47702c",
-			measurementId: "G-VFGSLK4GP4"
+   			  apiKey: 'api-key',
+			  authDomain: 'project-id.firebaseapp.com',
+			  databaseURL: 'https://project-id.firebaseio.com',
+			  projectId: 'project-id',
+			  storageBucket: 'project-id.appspot.com',
+			  messagingSenderId: 'sender-id',
+			  appId: 'app-id',
+			  measurementId: 'G-measurement-id',
 };
 
 firebase.initializeApp(firebaseConfig);
